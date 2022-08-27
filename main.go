28fa25449d0ca277f9cb32e49f432c87e1ee0004
Braian Nunes <braiannunes10@gmail.com>
2022-08-27 22:39:23 +0200

    Scraping Candy Store and returning a json

--- main.go
+++ main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
 	"log"
 	"net/http"
+	"sort"
 	"strconv"
 )
 
-// CandyStore Create a struct that represents a candy store
 type CandyStore struct {
 	Name  string
 	Candy string
 	Eaten int
-	Total int
 }
 
 type FavoriteSnack struct {
-	Name          string
-	FavoriteSnack string
-	totalSnacks   int
+	Name          string `json:"name"`
+	FavoriteSnack string `json:"favoriteSnack"`
+	TotalSnacks   int    `json:"totalSnacks"`
 }
 
-func ExampleScrape() {
-	res, err := http.Get("https://candystore.zimpler.net/")
+func OrderFavoriteSnacksByTotalSnacksDescending(favoriteSnacks []FavoriteSnack) []FavoriteSnack {
+	sort.SliceStable(favoriteSnacks, func(i, j int) bool {
+		return favoriteSnacks[i].TotalSnacks > favoriteSnacks[j].TotalSnacks
+	})
+	return favoriteSnacks
+}
+
+func ScrapeCandyStorePage() []CandyStore {
+	const URL = "https://candystore.zimpler.net"
+
+	res, err := http.Get(URL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,148 +45,99 @@ func ExampleScrape() {
 		}
 	}(res.Body)
 
-	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Create a list to hold the candy stores
 	var candyStores = make([]CandyStore, 0)
 
-	// Find all links and process them with the function
-	// defined earlier
-	// Find a table with id "top.customers"
 	doc.Find("table[id='top.customers']").Each(func(i int, item *goquery.Selection) {
 		item.Find("tbody tr").Each(func(index int, item *goquery.Selection) {
 			name := item.Find("td:nth-child(1)").Text()
 			candy := item.Find("td:nth-child(2)").Text()
 			eaten, _ := strconv.Atoi(item.Find("td:nth-child(3)").Text())
 
-			// Create a new instance of the struct
 			var candyStore = CandyStore{
 				Name:  name,
 				Candy: candy,
 				Eaten: eaten,
 			}
-
-			// Append the new instance to the list
 			candyStores = append(candyStores, candyStore)
 		})
 	})
 
-	// Print the list of candy stores
-	//PrintCandyStoreList(candyStores)
-
-	GroupCandyStoreByCandy(candyStores)
-
-	//GroupCandyStoreByName(candyStores)
-}
-
-func GroupCandyStoreByName(stores []CandyStore) {
-	// Group the candy stores by name
-	nameMap := make(map[string][]CandyStore)
-	for _, candyStore := range stores {
-		nameMap[candyStore.Name] = append(nameMap[candyStore.Name], candyStore)
+	if len(candyStores) == 0 {
+		log.Fatal("No candy stores found")
 	}
 
-	// Print the candy stores grouped by name
-	for name, candyStores := range nameMap {
-		fmt.Println("Name: ", name)
-		for _, candyStore := range candyStores {
-			// Printf with a new line
-			fmt.Printf("Name: %s, Eaten: %d, Candy: %s, \n", candyStore.Name, candyStore.Eaten, candyStore.Candy)
+	return candyStores
+}
+
+func GetFavoriteSnack(groupedByName map[string][]CandyStore) []FavoriteSnack {
+	var favoriteSnacks = make([]FavoriteSnack, 0)
+
+	for name, candyStores := range groupedByName {
+		var favoriteSnack = FavoriteSnack{
+			Name:          name,
+			FavoriteSnack: "",
+			TotalSnacks:   0,
 		}
+
+		for _, candyStore := range candyStores {
+			if candyStore.Eaten > favoriteSnack.TotalSnacks {
+				favoriteSnack.FavoriteSnack = candyStore.Candy
+			}
+			favoriteSnack.TotalSnacks += candyStore.Eaten
+		}
+
+		favoriteSnacks = append(favoriteSnacks, favoriteSnack)
 	}
+	return favoriteSnacks
 }
 
-func PrintCandyStoreList(candyStores []CandyStore) {
-	for _, candyStore := range candyStores {
-		fmt.Printf("Name: %s, Eaten: %d, Candy: %s \n", candyStore.Name, candyStore.Eaten, candyStore.Candy)
-	}
-}
-
-func GroupCandyStoreByCandy(stores []CandyStore) {
-	// Group the candy stores by candy
+func GroupCandyStoreByCandy(stores []CandyStore) map[string][]CandyStore {
 	nameMap := make(map[string][]CandyStore)
-	//map2 := make(map[string][]CandyStore)
-
-	// Calculate the total of totalSnacks by candy
 
 	for _, candyStore := range stores {
-		// Check if the Name is already in the map
 		if _, ok := nameMap[candyStore.Name]; !ok {
 			nameMap[candyStore.Name] = append(nameMap[candyStore.Name], candyStore)
 		} else {
-			// change the value of Name in nameMap[candyStore.Name]
 			var list = nameMap[candyStore.Name]
 
-			var foundNameInList = false
+			var isCandyInTheList = false
 			for idx, candy := range list {
-				// ge the pointer to the candyStore
 				if candy.Candy == candyStore.Candy {
 					list[idx].Eaten += candyStore.Eaten
-
-					fmt.Println("Found candy in list")
-					foundNameInList = true
+					isCandyInTheList = true
 					break
 				}
 			}
-
-			if !foundNameInList {
+			if !isCandyInTheList {
 				nameMap[candyStore.Name] = append(nameMap[candyStore.Name], candyStore)
 			}
 		}
 	}
 
-	//candyMap := make(map[string][]CandyStore)
-	//Do a for in nameMap
-	//for _, candyStores := range nameMap {
-	//	Group the candy stores by candy
-	//for _, candyStore := range candyStores {
-	//	candyMap[candyStore.Candy] = append(candyMap[candyStore.Candy], candyStore)
-	//}
-	//}
-
-	// Print the candy stores grouped by candy
-	for name, candyStores := range nameMap {
-		fmt.Println("Name: ", name)
-		for _, candyStore := range candyStores {
-			// Printf with a new line
-			fmt.Printf("Name: %s, Candy: %s, Eaten: %d\n", candyStore.Name, candyStore.Candy, candyStore.Eaten)
-		}
-	}
-
+	return nameMap
 }
 
 func main() {
-	ExampleScrape()
-	//TestPointers()
-}
+	// Scrape the candy store page
+	candyStores := ScrapeCandyStorePage()
 
-func TestPointers() {
-	// Create a new instance of the struct
-	var candyStore = CandyStore{
-		Name:  "Zimpler",
-		Candy: "Gummy Bears",
-		Eaten: 0,
-		Total: 0,
-	}
+	// Group the candy stores by name
+	var groupedByCandy = GroupCandyStoreByCandy(candyStores)
 
-	// Print the struct
-	fmt.Println(candyStore)
+	// Get the favorite snack for each person
+	favoriteSnacks := GetFavoriteSnack(groupedByCandy)
 
-	// Add the struct to a list
-	var candyStores = make([]CandyStore, 0)
-	candyStores = append(candyStores, candyStore)
+	// Order the favorite snacks by total snacks descending
+	OrderFavoriteSnacksByTotalSnacksDescending(favoriteSnacks)
 
-	fmt.Println(candyStores)
+	// Convert the favorite snacks to json
+	data, _ := json.MarshalIndent(favoriteSnacks, "", " ")
 
-	// Edit the struct
-	var candyStore2 = &candyStores[0]
-	candyStore2.Eaten = 1
-	candyStore2.Total = 1
-
-	fmt.Println(candyStores)
-
+	// Print the favorite snacks
+	fmt.Println(string(data))
 }
